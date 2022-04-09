5144e8699055e8419e3880314cad2f62fbcad648
Zubair Siddique <zubairsidd20@gmail.com>
2022-04-09 21:12:23 +0530

    minor change

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("/about", (req, res) => {
   res.render("about-us");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
